7e5741f7d20b698c00748cbdaa9b4926f038f51d
Gabriel <Gabriel@Prometheus.home>
2018-10-26 03:15:28 +0200

    Perita pero falta transferencia

--- Boletin_5_1/src/boletin_5_1/Boletin_5_1.java
+++ Boletin_5_1/src/boletin_5_1/Boletin_5_1.java
@@ -2,60 +2,70 @@ package boletin_5_1;
 import java.util.Scanner;
 
 public class Boletin_5_1{
-
+    
+    private static Scanner leer = new Scanner(System.in);
+    private static Conta cuenta1 = new Conta("Gabriel","1234",100);
+    private static Conta cuenta2 = new Conta("Belfegor","666",666);
+    
     private static int menuBanco(){
-        Scanner leer = new Scanner(System.in);
         int opcion = 0;
-        
-            System.out.println("\n"
-                    + "[1] Ver datos de su cuenta bancaria\n"
-                    + "[2] Ingresar dinero\n"
-                    + "[3] Retirar dinero\n"
-                    + "[4] Realizar una transferencia\n"
-                    + "[0] Salir");
-            
-            try{
-                opcion = Integer.parseInt(leer.nextLine());
-                
-            }catch(NumberFormatException e){
-                opcion = -1;
-                
-            }
-        
-        return opcion;
-       
+
+        System.out.println("\n  __  __                         _         _           _ \n" +
+" |  \\/  |___ _ _ _  _   _ __ _ _(_)_ _  __(_)_ __ __ _| |\n" +
+" | |\\/| / -_| ' | || | | '_ | '_| | ' \\/ _| | '_ / _` | |\n" +
+" |_|  |_\\___|_||_\\_,_| | .__|_| |_|_||_\\__|_| .__\\__,_|_|\n" +
+"                       |_|                  |_|          \n"
+                + "\n[1] Ver datos de su cuenta bancaria.\n"
+                + "[2] Ingresar dinero.\n"
+                + "[3] Retirar dinero.\n"
+                + "[4] Realizar una transferencia.\n"
+                + "[0] Salir.");
+        try{
+            opcion = Integer.parseInt(leer.nextLine());
+        }catch(NumberFormatException e){
+            opcion = -1;
+        }
+    return opcion;
     }
 
     public static void main(String[] args){
-        Scanner leer = new Scanner(System.in);
-        
-        int opcion;
-        
-        Conta cuenta1 = new Conta("Gabriel","1234",100);
-        Conta cuenta2 = new Conta("Belfegor","666",666);
-        
         System.out.println("[*] Bienvenido a su cuenta bancaria. \n[*] Porfavor, seleccione una opción de las que aparecen a continuación:");
-        
+        int opcion;
         do{
-            
-            switch(opcion = menuBanco()){
-                case 1: cuenta1.visualizar(); break;
-                case 2: 
-                    System.out.print("\n[-] Introduce la cantidad que deseas ingresar: "); 
-                    double c = leer.nextInt();
-                    cuenta1.ingresar(c);
+            opcion = menuBanco();
+            switch(opcion){
+                case 1:
+                    System.out.print("\n[*] Detalles de su cuenta bancaria: ");
+                    cuenta1.visualizar(); break;
+                case 2:
+                    System.out.print("\n[-] Introduzca la cantidad que desea ingresar: ");
+                    try{
+                        if(cuenta1.ingresar(Integer.parseInt(leer.nextLine())) == true){
+                            System.out.println("\n[*] Operación realizada correctamente.");
+                        }else{
+                            System.out.println("\n[*] Operación fallida. Porfavor, inténtelo de nuevo.");
+                        }
+                    }catch(NumberFormatException e){
+                        System.out.println("\n[*] Formato incorrecto.");
+                    }
                     break;
-                case 3: break;
+                case 3:
+                    System.out.print("\n[-] Introduzca la cantidad que desea retirar: ");
+                    try{
+                        if(cuenta1.retirar(Integer.parseInt(leer.nextLine())) == true){
+                            System.out.println("\n[*] Operación realizada correctamente.");
+                        }else{
+                            System.out.println("\n[*] Operación fallida. Porfavor, inténtelo de nuevo.");
+                        }
+                    }catch(NumberFormatException e){
+                        System.out.println("\n[*] Formato incorrecto.");
+                    }
+                    break;
                 case 4: break;
                 case 0: System.out.println("\n[*] ¡Hasta luego!"); break;
-                default: System.out.println("\n[*] Error: Formato incorrecto"); break;
-                
+                default: System.out.println("\n[*] Formato incorrecto."); break;
             }
-            
-        }while(opcion != 0);
-            
+        }while(opcion != 0);   
     }
-        
-
-        
+   
 }
--- Boletin_5_1/src/boletin_5_1/Conta.java
+++ Boletin_5_1/src/boletin_5_1/Conta.java
@@ -1,91 +1,76 @@
 package boletin_5_1;
 
-public class Conta {
-    private String nomeConta;
-    private String numeroConta;
+public class Conta{
+    private String nomeConta,numeroConta;
     private double saldoConta;
     
     //Constructor por defecto
-    public Conta() {
-        
+    public Conta(){
     }
     
     //Constructor con parametros
-    public Conta(String nomeConta, String numeroConta, double saldoConta) {
+    public Conta(String nomeConta, String numeroConta, double saldoConta){
         this.nomeConta = nomeConta;
         this.numeroConta = numeroConta;
         this.saldoConta = saldoConta;
-          
     }
     
     //Setters
-    public void setSaldo(double saldoConta) {
+    public void setSaldo(double saldoConta){
         this.saldoConta = saldoConta;
-        
     }
     
-    public void setNumero(String numeroConta) {
+    public void setNumero(String numeroConta){
         this.numeroConta = numeroConta;
     }
     
-    public void setNome(String nomeConta) {
+    public void setNome(String nomeConta){
         this.nomeConta = nomeConta;
     }
     
     //Getters
-    public double getSaldo() {
-        return this.saldoConta;
-        
+    public double getSaldo(){
+        return this.saldoConta;  
     }
     
-    public String getNumero() {
+    public String getNumero(){
         return this.numeroConta;
-        
     }
     
-    public String getNome() {
+    public String getNome(){
         return this.nomeConta;
-        
     }
     
     //Métodos
-    public boolean ingresar(double cantidade) {
+    public boolean ingresar(double cantidade){
         if(cantidade < 0) {
             return false;
-            
-        }else {
+        }else{
             saldoConta += cantidade;
             return true;
-            
         }
-        
     }
     
-    public boolean reintegrar(double cantidade) {
-        if(saldoConta - cantidade < 0) {
+    public boolean retirar(double cantidade){
+        if(saldoConta - cantidade < 0){
             return false;
-            
         }else {
             if(cantidade < 0){
                 return false;
-                
             }else {
                 saldoConta -= cantidade;
                 return true;
-                        
             }
         }
     }
     
     public void visualizar(){
-        System.out.println("\n- Número de cuenta: " + numeroConta + "\n- Nombre del cliente: " + nomeConta + "\n- Saldo disponible: " + saldoConta + " euros");
-        
+        System.out.println("\n- Número de cuenta:\t" + numeroConta + ".\n- Nombre del cliente:\t" + nomeConta + ".\n- Saldo disponible:\t" + saldoConta + " euros.");
     }
     
     public void transferencia(String cuentaDestino, double importe){
         this.saldoConta -= importe;
         cuentaDestino += importe; //????????
-        
     }
     
 }
